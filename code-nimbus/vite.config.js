bf9c5631b4a827ac8aeba6f50e55887f360477b6
harsha1010 <harshasrinivas1010@gmail.com>
2024-01-08 03:38:24 +0530

    ed

--- code-nimbus/vite.config.js
+++ code-nimbus/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  // base:"/code_nimbus/",
   plugins: [react()],
-  base:"/code_nimbus/"
+ 
 })
